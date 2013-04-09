156af1a3ad446ae881dac373183f3ab87ca162eb
vtourraine <me@vtourraine.net>
2013-04-09 16:11:43 +0200

    Add new labels to iOS demo app interface

--- Examples/SZNAltmetricDemo/SZNAltmetricDemo/classes/SZNArticleViewController.h
+++ Examples/SZNAltmetricDemo/SZNAltmetricDemo/classes/SZNArticleViewController.h
@@ -18,4 +18,24 @@
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
+@property (weak, nonatomic) IBOutlet UILabel *altmetricIdentifierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *DOILabel;
+@property (weak, nonatomic) IBOutlet UILabel *arXivLabel;
+@property (weak, nonatomic) IBOutlet UILabel *NLMIdentifierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pubMedIdentifierLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ADSBibcodeLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *journalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *publishedOnLabel;
+@property (weak, nonatomic) IBOutlet UILabel *lastUpdatedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *addedOnLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *articleURLLabel;
+@property (weak, nonatomic) IBOutlet UILabel *detailsURLLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *tweetersLabel;
+@property (weak, nonatomic) IBOutlet UILabel *facebookUsersLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scienceBlogsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *googlePlusLabel;
+
 @end
