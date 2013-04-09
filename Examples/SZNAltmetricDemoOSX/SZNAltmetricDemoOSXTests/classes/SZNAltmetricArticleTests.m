312e80160851fe5cc934276d3fde93b34b174ea8
vtourraine <me@vtourraine.net>
2013-04-09 14:43:10 +0200

    Move example JSON article for unit testing to separate file

--- Examples/SZNAltmetricDemoOSX/SZNAltmetricDemoOSXTests/classes/SZNAltmetricArticleTests.m
+++ Examples/SZNAltmetricDemoOSX/SZNAltmetricDemoOSXTests/classes/SZNAltmetricArticleTests.m
@@ -13,56 +13,38 @@
 
 - (void)testArticleFromRespondeDictionary
 {
-    SZNAltmetricArticle *article = [SZNAltmetricArticle articleWithAPIResponseObject:@{
-                                    @"added_on" : @(1324491937),
-                                    @"altmetric_id" : @(1),
-                                    @"ads_id" : @"ADS-123",
-                                    @"cited_by_accounts_count" : @(393),
-                                    @"cited_by_fbwalls_count" : @(12),
-                                    @"cited_by_feeds_count" : @(2),
-                                    @"cited_by_gplus_count" : @(20),
-                                    @"cited_by_posts_count" : @(402),
-                                    @"cited_by_tweeters_count" : @(359),
-                                    @"doi" : @"DOI-123",
-                                    @"arxiv_id" : @"arXiv-123",
-                                    @"last_updated" : @(1324491938),
-                                    @"nlmid" : @"NLM-123",
-                                    @"pmid" : @"pmid-123",
-                                    @"published_on" : @(1324491939),
-                                    @"score" : @(42),
-                                    @"images" : @{@"small" : @"http://test.com/small.jpg", @"medium" : @"http://test.com/medium.jpg", @"large" : @"http://test.com/large.jpg"},
-                                    @"is_oa" : @(YES),
-                                    @"journal": @"Jou",
-                                    @"title" : @"Article",
-                                    @"details_url" : @"http://test.com/article/details",
-                                    @"url" : @"http://test.com/article"}];
+    NSData *JSONData = [NSData dataWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:@"article" ofType:@"json"]];
+    NSError *JSONError = nil;
+    NSDictionary *JSONArticleDictionary = [NSJSONSerialization JSONObjectWithData:JSONData options:kNilOptions error:&JSONError];
+    SZNAltmetricArticle *article = [SZNAltmetricArticle articleWithAPIResponseObject:JSONArticleDictionary];
     
+    STAssertNil(JSONError, @"Article deserialization failed");
     STAssertNotNil(article, @"Article not created");
     
     STAssertEqualObjects(article.addedOn, [NSDate dateWithTimeIntervalSince1970:1324491937], @"Added on not extracted");
-    STAssertEqualObjects(article.identifier, @"1", @"Identifier not extracted");
-    STAssertEqualObjects(article.ADSBibcode, @"ADS-123", @"ADS not extracted");
+    STAssertEqualObjects(article.identifier, @"502878", @"Identifier not extracted");
+    STAssertEqualObjects(article.ADSBibcode, @"2011natur.480..426v", @"ADS not extracted");
     STAssertEqualObjects(article.citedByAccountsCount, @(393), @"Cited by accounts not extracted");
     STAssertEqualObjects(article.citedByFacebookWallsCount, @(12), @"Cited by Facebook walls not extracted");
     STAssertEqualObjects(article.citedByFeedsCount, @(2), @"Cited by feeds not extracted");
     STAssertEqualObjects(article.citedByGooglePlusCount, @(20), @"Cited by Google+ not extracted");
     STAssertEqualObjects(article.citedByPostsCount, @(402), @"Cited by posts not extracted");
     STAssertEqualObjects(article.citedByTweetersCount, @(359), @"Cited by tweeters not extracted");
-    STAssertEqualObjects(article.DOI, @"DOI-123", @"DOI not extracted");
+    STAssertEqualObjects(article.DOI, @"10.1038/480426a", @"DOI not extracted");
     STAssertEqualObjects(article.arXiv, @"arXiv-123", @"arXiv identifier not extracted");
-    STAssertEqualObjects(article.lastUpdated, [NSDate dateWithTimeIntervalSince1970:1324491938], @"Last updated not extracted");
-    STAssertEqualObjects(article.NLMIdentifier, @"NLM-123", @"NLM identifier not extracted");
-    STAssertEqualObjects(article.pubMedIdentifier, @"pmid-123", @"PubMed identifier not extracted");
-    STAssertEqualObjects(article.publishedOn, [NSDate dateWithTimeIntervalSince1970:1324491939], @"Published on not extracted");
-    STAssertEqualObjects(article.score, @(42), @"Score not extracted");
-    STAssertEqualObjects(article.imageSmallURL.absoluteString, @"http://test.com/small.jpg", @"Image small URL not extracted");
-    STAssertEqualObjects(article.imageMediumURL.absoluteString, @"http://test.com/medium.jpg", @"Image medium URL not extracted");
-    STAssertEqualObjects(article.imageLargeURL.absoluteString, @"http://test.com/large.jpg", @"Image large URL not extracted");
-    STAssertEqualObjects(article.openAccess, @(YES), @"Open Access not extracted");
-    STAssertEqualObjects(article.journal, @"Jou", @"Journal not extracted");
-    STAssertEqualObjects(article.title, @"Article", @"Title not extracted");
-    STAssertEqualObjects(article.articleURL.absoluteString, @"http://test.com/article", @"Article URL not extracted");
-    STAssertEqualObjects(article.detailsURL.absoluteString, @"http://test.com/article/details", @"Details URL not extracted");
+    STAssertEqualObjects(article.lastUpdated, [NSDate dateWithTimeIntervalSince1970:1353885698], @"Last updated not extracted");
+    STAssertEqualObjects(article.NLMIdentifier, @"0410462", @"NLM identifier not extracted");
+    STAssertEqualObjects(article.pubMedIdentifier, @"22193080", @"PubMed identifier not extracted");
+    STAssertEqualObjects(article.publishedOn, [NSDate dateWithTimeIntervalSince1970:1324512000], @"Published on not extracted");
+    STAssertEqualObjects(article.score, @(353.626), @"Score not extracted");
+    STAssertEqualObjects(article.imageSmallURL.absoluteString, @"http://fastly.altmetric.com/?size=64&score=354&types=bfgttttt", @"Image small URL not extracted");
+    STAssertEqualObjects(article.imageMediumURL.absoluteString, @"http://fastly.altmetric.com/?size=100&score=354&types=bfgttttt", @"Image medium URL not extracted");
+    STAssertEqualObjects(article.imageLargeURL.absoluteString, @"http://fastly.altmetric.com/?size=180&score=354&types=bfgttttt", @"Image large URL not extracted");
+    STAssertEqualObjects(article.openAccess, @(NO), @"Open Access not extracted");
+    STAssertEqualObjects(article.journal, @"Nature", @"Journal not extracted");
+    STAssertEqualObjects(article.title, @"365 days: 2011 in review", @"Title not extracted");
+    STAssertEqualObjects(article.articleURL.absoluteString, @"http://www.nature.com/news/365-days-2011-in-review-1.9684", @"Article URL not extracted");
+    STAssertEqualObjects(article.detailsURL.absoluteString, @"http://www.altmetric.com/details.php?citation_id=502878", @"Details URL not extracted");
 }
 
 @end
